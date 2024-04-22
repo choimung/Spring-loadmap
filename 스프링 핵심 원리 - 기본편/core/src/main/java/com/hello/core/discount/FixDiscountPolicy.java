dfc67bc896803179f908f1ddc29fc60899dea8f7
choimung <103022503+choimung@users.noreply.github.com>
2024-04-22 23:17:53 +0900

    feat : 스프링 핵심 원리 - 완료

--- 스프링 핵심 원리 - 기본편/core/src/main/java/com/hello/core/discount/FixDiscountPolicy.java
+++ 스프링 핵심 원리 - 기본편/core/src/main/java/com/hello/core/discount/FixDiscountPolicy.java
@@ -2,9 +2,11 @@ package com.hello.core.discount;
 
 import com.hello.core.member.Grade;
 import com.hello.core.member.Member;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class FixDiscountPolicy implements DiscountPolicy{
 
     private final int discountFixAmount = 1000; // 1,000원 할인
