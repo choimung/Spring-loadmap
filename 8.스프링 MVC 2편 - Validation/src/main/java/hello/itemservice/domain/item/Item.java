590eb8bad14d5326060cd40a8f15fed2d734c383
choimung <103022503+choimung@users.noreply.github.com>
2024-05-02 23:19:56 +0900

    feat : [섹션 5 BeanValidation ] 완료

--- 8.스프링 MVC 2편 - Validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ 8.스프링 MVC 2편 - Validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,28 @@
 package hello.itemservice.domain.item;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총 합이 10,000원 넘게 주문하쇼")
 public class Item {
 
+//    @NotNull(groups = UpdateCheck.class)
     private Long id;
+
+//    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
+
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1000, max = 100000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
+
+//    @NotNull(groups = UpdateCheck.class)
+//    @Max(value = 9999, groups = {SaveCheck.class})
     private Integer quantity;
 
     public Item() {
