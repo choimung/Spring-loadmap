35627d56828037ca18026affc9f775c1131b73dd
choimung <103022503+choimung@users.noreply.github.com>
2024-05-15 11:01:47 +0900

    feat :[데이터 접근 기술 - 스프링 데이터 JPA] - 완료

---
+++ 14.스프링 DB 2편 - 데이터 접근 활용 기술/src/main/java/hello/itemservice/repository/jpa/SpringDataJpaItemRepository.java
@@ -0,0 +1,19 @@
+package hello.itemservice.repository.jpa;
+
+import hello.itemservice.domain.Item;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface SpringDataJpaItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findByItemName(@Param("itemName") String itemName);
+    List<Item> findByPriceLessThanEqual(@Param("price")Integer price);
+
+    //쿼리 메서드
+    List<Item> findByItemNameLikeAndPriceLessThanEqual(String itemName, Integer price);
+
+    @Query("select i from Item i where i.itemName like :itemName and i.price <= :price")
+    List<Item> findItems(@Param("itemName") String itemName, @Param("price") Integer price);
+
+}
